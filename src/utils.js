64570b49668814e5c1d05bf3bf096bfa5f8f7032
Vitalis Ian <viterlis254@gmail.com>
2024-08-12 21:28:19 +0300

    initial

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
-// Your code here
+// utils.js
+
+/**
+ * Calculates the current age based on the provided birth year.
+ * @param {number} 
+ * @returns {number} 
+ */
+export function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+  }
+  
